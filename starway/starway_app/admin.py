6347e2a8c48397285c3d0492f2866d9c222795e5
christian <chrisalv27@yahoo.com>
2022-02-10 16:22:28 -0800

    inputing data

--- starway/starway_app/admin.py
+++ starway/starway_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User,Zodiac
+
+admin.site.register(User)
+admin.site.register(Zodiac)
+
 
 # Register your models here.
--- starway/starway_app/models.py
+++ starway/starway_app/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
-class User(models.model):
+class User(models.Model):
     name = models.CharField(max_length=20)
     birth = models.DateField(max_length=10)
 
 
-class zodiac(models.model):
+class Zodiac(models.Model):
     name = models.CharField(max_length=10)
-    signs = models.ManyToManyField('sign')
+    matches = models.ManyToManyField('Zodiac', blank=True)
+    element = models.CharField(max_length=10)
+    description = models.CharField(max_length=500)
+    planet = models.CharField(max_length=10,null=True)
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
