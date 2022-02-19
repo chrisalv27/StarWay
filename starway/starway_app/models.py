26968c2dd5ee9a7fe11070e71340c6cc82a135ec
christian <chrisalv27@yahoo.com>
2022-02-18 16:10:25 -0800

    starway

--- starway/starway_app/models.py
+++ starway/starway_app/models.py
@@ -4,6 +4,10 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     birth = models.DateField(max_length=10)
     zodiac = models.ForeignKey('Zodiac', on_delete=models.PROTECT, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.name} {self.zodiac}'
+        return self.name + str(self.zodiac) # return self.name + self.zodiac.name
 
 class Zodiac(models.Model):
     name = models.CharField(max_length=25)
--- starway/starway_app/views.py
+++ starway/starway_app/views.py
@@ -12,20 +12,82 @@ from django.http import HttpResponse
 def getsign(request):
     if request.method == 'POST':
         birth = request.POST.get('birth')
-        # name = ...get name somehow
-        user = User.objects.create(birth=birth, name='Bob')
-        print(user.name, user.birth)
-        user_bday_2022 = user.birth.replace(year=2022)
-        zodiacs = Zodiac.objects.all()
-        for zodiac in zodiacs:
-            print()
-            print(zodiac.name)
-            print(zodiac.start_date)# <= user_bday_2022)
-            print(zodiac.end_date)# >= user_bday_2022)
-            # if zodiac.name == 'Aquarius': # in your version it would be more like if user.birth is in the range
-                                          # of the zodiac start and end dates
-                # user.zodiac = zodiac
+        # name = request.POST.get('name')
+        user = User.objects.create(birth=birth, name='bob')
+        
+        temp_date = user.birth
+        temp_year = '2022-'
+        for i in range(5,10):
+            temp_year += temp_date[i]
+        
+        day = user.birth[8] + user.birth[9] #using indices of the day colum, concatinate them into day ex. "07"
+        day = int(day) #typecast day string into an integer, to compare it with greater than or less than
+        if user.birth[5] == "0" and user.birth[6] == "1":
+                if day < 20:
+                    user.zodiac = Zodiac.objects.get(name='Capricorn')
+                elif day >= 20:
+                    user.zodiac = Zodiac.objects.get(name='Aquarius')
+        if user.birth[5] == "0" and user.birth[6] == "2":
+                if day < 18:
+                    user.zodiac = Zodiac.objects.get(name='Aquarius')
+                elif day >= 18:
+                    user.zodiac = Zodiac.objects.get(name='Pisces')
+        if user.birth[5] == "0" and user.birth[6] == "3":
+                if day < 20:
+                    user.zodiac = Zodiac.objects.get(name='Pisces')
+                elif day >= 20:
+                    user.zodiac = Zodiac.objects.get(name='Aries')
+        if user.birth[5] == "0" and user.birth[6] == "4":
+                if day < 19:
+                    user.zodiac = Zodiac.objects.get(name='Aries')
+                elif day >= 19:
+                    user.zodiac = Zodiac.objects.get(name='Taurus')
+        if user.birth[5] == "0" and user.birth[6] == "5":
+                if day < 20:
+                    user.zodiac = Zodiac.objects.get(name='Taurus')
+                elif day >= 20:
+                    user.zodiac = Zodiac.objects.get(name='Gemini')
+        if user.birth[5] == "0" and user.birth[6] == "6":
+                if day < 21:
+                    user.zodiac = Zodiac.objects.get(name='Gemini')
+                elif day >= 21:
+                    user.zodiac = Zodiac.objects.get(name='Cancer')
+        if user.birth[5] == "0" and user.birth[6] == "7":
+                if day < 22:
+                    user.zodiac = Zodiac.objects.get(name='Cancer')
+                elif day >= 22:
+                    user.zodiac = Zodiac.objects.get(name='Leo')
+        if user.birth[5] == "0" and user.birth[6] == "8":
+                if day < 22:
+                    user.zodiac = Zodiac.objects.get(name='Leo')
+                elif day >= 22:
+                    user.zodiac = Zodiac.objects.get(name='Virgo')
+        if user.birth[5] == "0" and user.birth[6] == "9":
+                if day < 22:
+                    user.zodiac = Zodiac.objects.get(name='Virgo')
+                elif day >= 22:
+                    user.zodiac = Zodiac.objects.get(name='Libra')
+        if user.birth[5] == "1" and user.birth[6] == "0":
+                if day < 23:
+                    user.zodiac = Zodiac.objects.get(name='Libra')
+                elif day >= 23:
+                    user.zodiac = Zodiac.objects.get(name='Scorpio')
+        if user.birth[5] == "1" and user.birth[6] == "1":
+                if day < 22:
+                    user.zodiac = Zodiac.objects.get(name='Scorpio')
+                elif day >= 22:
+                    user.zodiac = Zodiac.objects.get(name='Sagittarius')
+        if user.birth[5] == "1" and user.birth[6] == "2":
+                if day < 21:
+                    user.zodiac = Zodiac.objects.get(name='Sagittarius')
+                elif day >= 21:
+                    user.zodiac = Zodiac.objects.get(name='Capricorn')
+                    
+        user.save() # this actually writes the changes to the database
+                
+        
         return render(request, 'starway_app/starway.html')
+    
 
 def index(request):
     context = {
